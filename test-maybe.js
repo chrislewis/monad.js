96cff2c34c7ec10126d54ad2cd70550b695e97ff
chrislewis <chris@thegodcode.net>
2010-10-01 16:56:46 -0400

    get now getOr

--- test-maybe.js
+++ test-maybe.js
@@ -13,18 +13,18 @@ assert.equal(nothing.map(function(v) {
   return 1;
 }), m.Nothing);
 
-assert.equal(nothing.get(5), 5);
+assert.equal(nothing.getOr(5), 5);
 
 /* Just */
 assert.equal(justOne.bind(function(v) {
   return m.maybe(v * 5);
-}).get(20), 5);
+}).getOr(20), 5);
 
 assert.equal(justOne.map(function(v) {
   return v * 5;
-}).get(20), 5);
+}).getOr(20), 5);
 
-assert.equal(justOne.get(5), 1);
+assert.equal(justOne.getOr(5), 1);
 
 /* ex monad constructor */
 var epicFail = function() { throw 'java'; }
@@ -32,4 +32,4 @@ assert.throws(epicFail);
 assert.equal(m.ex(epicFail)(), m.Nothing);
 
 var identity = function(v) { return v; };
-assert.equal(m.ex(identity)(5).map(identity).get(1), 5);
+assert.equal(m.ex(identity)(5).map(identity).getOr(1), 5);
