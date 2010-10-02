1e1cca6414dca77a34a19abfb78f0a0262749fee
chrislewis <chris@thegodcode.net>
2010-10-02 19:02:41 -0400

    packaging for npm

---
+++ index.js
@@ -0,0 +1,4 @@
+// This file is just added for convenience so this repository can be
+// directly checked out into a project's deps folder.
+// Idea from Caolan McMahon/async (http://github.com/caolan)
+module.exports = require('./lib/monad');
